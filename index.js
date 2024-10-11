5c8d30be66d277643417208fc1b9ed4b11ba596b
KunalNT <kunal@nichetech.in>
2024-10-11 18:44:46 +0530

    fourth  commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+// const express = require("express");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+// const qrcodeRoutes = require("./routes/qrRoutes.js");
+
+// const app = express();
+// const Port = 8000;
+
+// // Connect to MongoDB
+// connectDB();
+
+// // Middleware
+// app.use(cors({ origin: "*" })); // Allow all origins
+// app.use(express.json()); // Parse JSON bodies
+
+// // Routes
+// app.use("/api/qrcodes", qrcodeRoutes);
+
+// // Start server
+// app.listen(Port, () => console.log(`Server running on port ${Port}`));
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const qrcodeRoutes = require("./routes/qrRoutes.js");
 
 const app = express();
-const Port = 8000;
 
 // Connect to MongoDB
 connectDB();
@@ -16,5 +36,7 @@ app.use(express.json()); // Parse JSON bodies
 // Routes
 app.use("/api/qrcodes", qrcodeRoutes);
 
-// Start server
-app.listen(Port, () => console.log(`Server running on port ${Port}`));
+// Export the function for Vercel
+module.exports = (req, res) => {
+  app(req, res); // Pass the request and response to the Express app
+};
